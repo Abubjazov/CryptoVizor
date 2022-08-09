66972b1db9319e5b2e936d4b5106d6b365c578aa
Radik Abubyazov <abubjazov@mail.ru>
2022-08-09 20:35:49 +0400

    PROJ: fixed Fade in effect

--- src/components/CryptoCurrency/CryptoCurrency.tsx
+++ src/components/CryptoCurrency/CryptoCurrency.tsx
@@ -1,17 +1,19 @@
 import React, { FC } from "react";
-import { Paper, Typography } from "@material-ui/core";
+import { Fade, Paper, Typography } from "@material-ui/core";
 
 import CryptoTable from "../CryptoTable";
 import { CryptoTableProps } from "../CryptoTable/CryptoTable";
 
 const CryptoCurrency: FC<CryptoTableProps> = ({ coins }) => {
   return (
-    <Paper elevation={1} style={{ height: "auto", paddingTop: "13px" }}>
-      <Typography style={{ marginLeft: "13px" }} variant="h5">
-        Курсы криптовалют
-      </Typography>
-      <CryptoTable coins={coins} />
-    </Paper>
+    <Fade in timeout={1000}>
+      <Paper elevation={1} style={{ height: "auto", paddingTop: "13px" }}>
+        <Typography style={{ marginLeft: "13px" }} variant="h5">
+          Курсы криптовалют
+        </Typography>
+        <CryptoTable coins={coins} />
+      </Paper>
+    </Fade>
   );
 };
 
--- src/components/CryptoTable/CryptoTable.tsx
+++ src/components/CryptoTable/CryptoTable.tsx
@@ -33,43 +33,41 @@ const CryptoTable: FC<CryptoTableProps> = ({ coins }) => {
   const classes = useStyles();
 
   return (
-    <Fade in timeout={1000}>
-      <TableContainer className={classes.tableContainer} component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell className={classes.tableHead}>Криптовалюта</TableCell>
-              <TableCell className={classes.tableHead} align="right">
-                Цена $
+    <TableContainer className={classes.tableContainer} component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className={classes.tableHead}>Криптовалюта</TableCell>
+            <TableCell className={classes.tableHead} align="right">
+              Цена $
+            </TableCell>
+            <TableCell className={classes.tableHead} align="right">
+              Куплено за 24 часа
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {coins.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                <div className={classes.coinWrapper}>
+                  <img
+                    src={"https://www.cryptocompare.com" + row.imageUrl}
+                    alt=""
+                    style={{ height: "31px", marginRight: "5px" }}
+                  />
+                  {row.fullName}
+                </div>
               </TableCell>
-              <TableCell className={classes.tableHead} align="right">
-                Куплено за 24 часа
+              <TableCell align="right">{Math.round(row.price)}</TableCell>
+              <TableCell align="right">
+                {Math.round(row.volume24Hour)}
               </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {coins.map((row) => (
-              <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  <div className={classes.coinWrapper}>
-                    <img
-                      src={"https://www.cryptocompare.com" + row.imageUrl}
-                      alt=""
-                      style={{ height: "31px", marginRight: "5px" }}
-                    />
-                    {row.fullName}
-                  </div>
-                </TableCell>
-                <TableCell align="right">{Math.round(row.price)}</TableCell>
-                <TableCell align="right">
-                  {Math.round(row.volume24Hour)}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Fade>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
