508e2bed249dbbb10f4f728d30f108a094a9f5c0
Radik Abubyazov <abubjazov@mail.ru>
2022-08-11 22:35:44 +0400

    Updates

---
+++ precache-manifest.c9f679217166c6c029f35153afe2e9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b365083e28775f23946c90f766fb7c6",
+    "url": "/CryptoVizor/index.html"
+  },
+  {
+    "revision": "86cca7da7a2830f24847",
+    "url": "/CryptoVizor/static/css/main.8e86772a.chunk.css"
+  },
+  {
+    "revision": "4a29664221cd4a7dad6c",
+    "url": "/CryptoVizor/static/js/2.adc4bdbf.chunk.js"
+  },
+  {
+    "revision": "bcea7869484a5f23b9d9ab4fe85414e3",
+    "url": "/CryptoVizor/static/js/2.adc4bdbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86cca7da7a2830f24847",
+    "url": "/CryptoVizor/static/js/main.acfa9922.chunk.js"
+  },
+  {
+    "revision": "83d0b5ef3ffab7fdfc15",
+    "url": "/CryptoVizor/static/js/runtime-main.5d2c207d.js"
+  }
+]);
